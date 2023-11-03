64283fe6233382e0956a9b5542bd1c9b7165436c
TrueRaisins <idanik.ishot@gmail.com>
2023-11-03 17:41:07 +0300

    initial commit

---
+++ src/main/java/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,25 @@
+package FizzBuzz;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+
+            if (i % (3 * 5) == 0) {
+
+                System.out.println(i + " " + "FIzzBuzz");
+                continue;
+            }
+            if (i % 3 == 0) {
+
+                System.out.println(i + " " + "Fizz");
+                continue;
+            }
+            if (i % 5 == 0) {
+
+                System.out.println(i + " " + "Buzz");
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
